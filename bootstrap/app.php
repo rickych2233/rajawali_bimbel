77f3627f5f7fc7fe0069458189db8a68e2cd5b85
ricky <rickychristian2309@gmail.com>
2026-08-25 12:20:55 +0700

    fix read only storage issue on vercel by using /tmp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +16,21 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL']) || getenv('VERCEL')) {
+    $app->useStoragePath('/tmp/storage');
+    $directories = [
+        'logs',
+        'framework/views',
+        'framework/cache/data',
+        'framework/sessions',
+    ];
+    foreach ($directories as $dir) {
+        $path = '/tmp/storage/' . $dir;
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+    }
+}
+
+return $app;
