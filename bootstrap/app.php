64e6c12826e2f283cbe7ca6be72d1039f2553f88
ricky <rickychristian2309@gmail.com>
2026-08-25 12:34:49 +0700

    Fix MaintenanceModeManager driver empty string issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,21 +14,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (\ArgumentCountError $e, \Illuminate\Http\Request $request) {
-            if (str_contains($e->getMessage(), 'createDriver')) {
-                $trace = $e->getTrace();
-                $classes = [];
-                foreach ($trace as $frame) {
-                    if (isset($frame['object'])) {
-                        $classes[] = get_class($frame['object']);
-                    } elseif (isset($frame['class'])) {
-                        $classes[] = $frame['class'];
-                    }
-                }
-                $classesList = implode(' -> ', array_unique($classes));
-                return response("<h1 style='color:red;'>VERCEL DEBUG: Trace classes: {$classesList}</h1><p>Please check your Vercel Environment Variables. One of the variables for this manager is set to an empty string in the Vercel Dashboard!</p>", 500);
-            }
-        });
+        //
     })->create();
 
 if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL']) || getenv('VERCEL')) {
