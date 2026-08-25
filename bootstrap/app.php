6aecf8d1a4c8b09015d5c9c5a79afff247217293
ricky <rickychristian2309@gmail.com>
2026-08-25 12:33:11 +0700

    Show entire trace classes for debugging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,8 +17,16 @@ $app = Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(function (\ArgumentCountError $e, \Illuminate\Http\Request $request) {
             if (str_contains($e->getMessage(), 'createDriver')) {
                 $trace = $e->getTrace();
-                $managerClass = isset($trace[0]['object']) ? get_class($trace[0]['object']) : ($trace[0]['class'] ?? 'Unknown');
-                return response("<h1 style='color:red;'>VERCEL DEBUG: The manager causing the error is: {$managerClass}</h1><p>Please check your Vercel Environment Variables. One of the variables for this manager is set to an empty string in the Vercel Dashboard!</p>", 500);
+                $classes = [];
+                foreach ($trace as $frame) {
+                    if (isset($frame['object'])) {
+                        $classes[] = get_class($frame['object']);
+                    } elseif (isset($frame['class'])) {
+                        $classes[] = $frame['class'];
+                    }
+                }
+                $classesList = implode(' -> ', array_unique($classes));
+                return response("<h1 style='color:red;'>VERCEL DEBUG: Trace classes: {$classesList}</h1><p>Please check your Vercel Environment Variables. One of the variables for this manager is set to an empty string in the Vercel Dashboard!</p>", 500);
             }
         });
     })->create();
